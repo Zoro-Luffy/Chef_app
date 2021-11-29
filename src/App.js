e84bf1fe40d49f0666eb7a5e903368ea710da6f7
Nekkanti <ruthch325@gmail.com>
2021-11-29 15:19:23 -0800

    chef app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="doNotRemoveMe">Hello world.</h1>
+      {/* ^ Do not remove this element ^ */}
+    </div>
+  );
+}
+
+export default App;
